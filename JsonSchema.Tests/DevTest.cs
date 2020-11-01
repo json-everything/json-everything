ff648156e45f25d34afd4d2e2acf1ffed065b039
Greg Dennis <gregsdennis@yahoo.com>
2020-11-01 13:23:42 +1300

    add dev test

---
+++ JsonSchema.Tests/DevTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+using NUnit.Framework;
+
+namespace Json.Schema.Tests
+{
+	public class DevTest
+	{
+		[Test]
+		public void Test()
+		{
+			var schema = JsonSchema.FromText(@"");
+			var instance = JsonDocument.Parse(@"").RootElement;
+
+			var results = schema.Validate(instance);
+
+			Console.WriteLine(JsonSerializer.Serialize(results));
+		}
+	}
+}
