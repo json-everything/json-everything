998194d772e45e9713822554a7fdc1bef7d15c66
Greg Dennis <gregsdennis@yahoo.com>
2020-11-22 21:08:20 +1300

    support for some array types

---
+++ JsonSchema.Generation.Tests/SchemaGenerationTests.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Json.Schema.Generation.Tests
+{
+	public class SchemaGenerationTests
+	{
+		public static IEnumerable<TestCaseData> SimpleTypeCases
+		{
+			get
+			{
+				yield return new TestCaseData(typeof(bool), SchemaValueType.Boolean);
+				yield return new TestCaseData(typeof(string), SchemaValueType.String);
+				yield return new TestCaseData(typeof(byte), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(short), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(ushort), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(int), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(uint), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(long), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(ulong), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(float), SchemaValueType.Number);
+				yield return new TestCaseData(typeof(double), SchemaValueType.Number);
+				yield return new TestCaseData(typeof(decimal), SchemaValueType.Number);
+			}
+		}
+
+		[TestCaseSource(nameof(SimpleTypeCases))]
+		public void CheckSimpleTypes(Type dotnetType, SchemaValueType schemaType)
+		{
+			JsonSchema expected = new JsonSchemaBuilder().Type(schemaType);
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType(dotnetType);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void IntArray()
+		{
+			JsonSchema expected = new JsonSchemaBuilder().Type(SchemaValueType.Array)
+				.Items(new JsonSchemaBuilder().Type(SchemaValueType.Integer));
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType<int[]>();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void ListOfStrings()
+		{
+			JsonSchema expected = new JsonSchemaBuilder().Type(SchemaValueType.Array)
+				.Items(new JsonSchemaBuilder().Type(SchemaValueType.String));
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType<List<string>>();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		// ReSharper disable once InconsistentNaming
+		public void IEnumerableOfListOfStrings()
+		{
+			JsonSchema expected = new JsonSchemaBuilder()
+				.Type(SchemaValueType.Array)
+				.Items(new JsonSchemaBuilder()
+					.Type(SchemaValueType.Array)
+					.Items(new JsonSchemaBuilder().Type(SchemaValueType.String))
+				);
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType<IEnumerable<List<string>>>();
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void GeneralArrayOfThings()
+		{
+			JsonSchema expected = new JsonSchemaBuilder().Type(SchemaValueType.Array);
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType<Array>();
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ JsonSchema.Generation/ArraySchemaGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Json.Schema.Generation
+{
+	internal class ArraySchemaGenerator : ISchemaGenerator
+	{
+		public bool Handles(Type type)
+		{
+			if (type.IsArray || type == typeof(Array)) return true;
+			if (!type.IsGenericType) return false;
+
+			var generic = type.GetGenericTypeDefinition();
+			return generic == typeof(IEnumerable<>) ||
+			       generic == typeof(List<>) ||
+			       generic == typeof(Stack<>) ||
+			       generic == typeof(Queue<>);
+		}
+
+		public void AddConstraints(JsonSchemaBuilder builder, Type type)
+		{
+			builder.Type(SchemaValueType.Array);
+
+			Type itemType = null;
+
+			if (type.IsGenericType)
+				itemType = type.GetGenericArguments().First();
+			else if (type.IsArray)
+				itemType = type.GetElementType();
+
+			if (itemType == null) return;
+
+			builder.Items(new JsonSchemaBuilder().FromType(itemType));
+		}
+	}
+}
