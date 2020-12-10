a43ee992725537b9a22be3ff307714dcc66a8a37
Greg Dennis <gregsdennis@yahoo.com>
2020-12-10 22:33:29 +1300

    added support for property name and ignore attributes

---
+++ JsonSchema.Generation/ObsoleteAttributeHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using JetBrains.Annotations;
+using Json.Schema.Generation.Intents;
+
+namespace Json.Schema.Generation
+{
+	[UsedImplicitly]
+	internal class ObsoleteAttributeHandler : IAttributeHandler
+	{
+		public void AddConstraints(SchemaGeneratorContext context)
+		{
+			if (context.Attributes.OfType<ObsoleteAttribute>().Any())
+				context.Intents.Add(new DeprecatedIntent(true));
+		}
+	}
+}
--- JsonSchema.Generation/SchemaGeneratorContext.cs
+++ JsonSchema.Generation/SchemaGeneratorContext.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Json.Pointer;
 using Json.Schema.Generation.Intents;
 
 namespace Json.Schema.Generation
