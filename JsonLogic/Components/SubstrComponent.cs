c0e666a8c14d46ee2144f82bfa20d82ef158d47d
Greg Dennis <gregsdennis@yahoo.com>
2020-12-17 22:25:53 +1300

    substr; tests

--- JsonLogic/Components/SubstrComponent.cs
+++ JsonLogic/Components/SubstrComponent.cs
@@ -1,12 +1,65 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
+using Json.More;
 
 namespace Json.Logic.Components
 {
 	internal class SubstrComponent : ILogicComponent
 	{
+		private readonly ILogicComponent _input;
+		private readonly ILogicComponent _start;
+		private readonly ILogicComponent _count;
+
+		public SubstrComponent(ILogicComponent input, ILogicComponent start)
+		{
+			_input = input;
+			_start = start;
+		}
+		public SubstrComponent(ILogicComponent input, ILogicComponent start, ILogicComponent count)
+		{
+			_input = input;
+			_start = start;
+			_count = count;
+		}
+
 		public JsonElement Apply(JsonElement data)
 		{
-			throw new System.NotImplementedException();
+			var input = _input.Apply(data);
+			var start = _start.Apply(data);
+			var count = _count?.Apply(data);
+
+			if (input.ValueKind != JsonValueKind.String)
+				throw new JsonLogicException($"Cannot substring a {input.ValueKind}.");
+
+			var stringInput = input.GetString();
+			
+			if (start.ValueKind != JsonValueKind.Number)
+				throw new JsonLogicException("Start value must be an integer");
+
+			var numberStart = start.GetDecimal();
+			if (numberStart != Math.Floor(numberStart))
+				throw new JsonLogicException("Start value must be an integer");
+
+			var intStart = (int) Math.Floor(numberStart);
+			if (intStart < -stringInput.Length) return input;
+			if (intStart < 0)
+				intStart = Math.Max(stringInput.Length + intStart, 0);
+			if (intStart >= stringInput.Length) return string.Empty.AsJsonElement();
+
+			if (count == null)
+				return stringInput[intStart..].AsJsonElement();
+
+			var numberCount = count.Value.GetDecimal();
+			if (numberCount != Math.Floor(numberCount))
+				throw new JsonLogicException("Count value must be an integer");
+
+			var availableLength = stringInput.Length - intStart;
+			var intCount = (int) Math.Floor(numberCount);
+			if (intCount < 0)
+				intCount = Math.Max(availableLength + intCount, 0);
+			intCount = Math.Min(availableLength, intCount);
+
+			return stringInput.Substring(intStart, intCount).AsJsonElement();
 		}
 	}
 }
