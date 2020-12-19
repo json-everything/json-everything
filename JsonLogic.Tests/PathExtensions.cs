5ef6c5670c5774ee85443a8a75402e8b96e4299f
Greg Dennis <gregsdennis@yahoo.com>
2020-12-20 00:23:59 +1300

    downloaded test file as backup

---
+++ JsonLogic.Tests/PathExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Json.Logic.Tests
+{
+	public static class PathExtensions
+	{
+		public static string AdjustForPlatform(this string path)
+		{
+			return Environment.OSVersion.Platform == PlatformID.MacOSX ||
+			       Environment.OSVersion.Platform == PlatformID.Unix
+				? path.Replace("\\", "/")
+				: path.Replace("/", "\\");
+		}
+	}
+}
--- JsonLogic.Tests/Suite/SuiteRunner.cs
+++ JsonLogic.Tests/Suite/SuiteRunner.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -14,11 +16,27 @@ namespace Json.Logic.Tests.Suite
 		{
 			return Task.Run(async () =>
 			{
-				using var client = new HttpClient();
-				using var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonlogic.com/tests.json");
-				using var response = await client.SendAsync(request);
+				var testsPath = System.IO.Path.Combine(TestContext.CurrentContext.WorkDirectory, "Files\\tests.json").AdjustForPlatform();
 
-				var content = await response.Content.ReadAsStringAsync();
+				string content = null;
+				try
+				{
+					using var client = new HttpClient();
+					using var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonlogic.com/tests.json");
+					using var response = await client.SendAsync(request);
+					
+					content = await response.Content.ReadAsStringAsync();
+					
+					await File.WriteAllTextAsync(testsPath, content);
+				}
+				catch (Exception e)
+				{
+					content ??= await File.ReadAllTextAsync(testsPath);
+
+					Console.WriteLine(e);
+				}
+
+
 				var testSuite = JsonSerializer.Deserialize<TestSuite>(content);
 
 				return testSuite.Tests.Select(t => new TestCaseData(t){TestName = $"{t.Logic}  |  {t.Data.ToJsonString()}  |  {t.Expected.ToJsonString()}"});
