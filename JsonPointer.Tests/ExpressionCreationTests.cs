1a9107c016fdc839e0f475fb54003aa34cbd088b
Greg Dennis <gregsdennis@yahoo.com>
2020-12-19 13:41:43 +1300

    added pointer creation from lambda expressions

---
+++ JsonPointer.Tests/ExpressionCreationTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Json.Pointer.Tests
+{
+	public class ExpressionCreationTests
+	{
+		class TestClass
+		{
+			public string String { get; set; }
+			public List<int> Ints { get; set; }
+			public TestClass Nest { get; set; }
+			public List<TestClass> NestMore { get; set; }
+		}
+
+		[Test]
+		public void SimpleProperty()
+		{
+			var expected = "/String";
+			var actual = JsonPointer.Create<TestClass>(x => x.String);
+			
+			Assert.AreEqual(expected, actual.ToString());
+		}
+
+		[Test]
+		public void SimpleArrayIndex()
+		{
+			var expected = "/Ints/1";
+			var actual = JsonPointer.Create<TestClass>(x => x.Ints[1]);
+			
+			Assert.AreEqual(expected, actual.ToString());
+		}
+
+		[Test]
+		public void NestedProperty()
+		{
+			var expected = "/Nest/Nest";
+			var actual = JsonPointer.Create<TestClass>(x => x.Nest.Nest);
+			
+			Assert.AreEqual(expected, actual.ToString());
+		}
+
+		[Test]
+		public void NestedWithArrayIndexProperty()
+		{
+			var expected = "/Nest/Ints/5";
+			var actual = JsonPointer.Create<TestClass>(x => x.Nest.Ints[5]);
+			
+			Assert.AreEqual(expected, actual.ToString());
+		}
+
+		[Test]
+		public void ArrayIndexWithNestProperty()
+		{
+			var expected = "/NestMore/5/Nest";
+			var actual = JsonPointer.Create<TestClass>(x => x.NestMore[5].Nest);
+			
+			Assert.AreEqual(expected, actual.ToString());
+		}
+	}
+}
