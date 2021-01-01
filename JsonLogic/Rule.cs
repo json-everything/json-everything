27b29758055d4c68a97f606e094ccd5649b7ad76
Greg Dennis <gregsdennis@yahoo.com>
2021-01-01 23:43:21 +1300

    xml comments

--- JsonLogic/Rule.cs
+++ JsonLogic/Rule.cs
@@ -7,23 +7,71 @@ using Json.Logic.Rules;
 
 namespace Json.Logic
 {
+	/// <summary>
+	/// Provides a base class for rules.
+	/// </summary>
 	[JsonConverter(typeof(LogicComponentConverter))]
 	public abstract class Rule
 	{
+		/// <summary>
+		/// Applies the rule to the input data.
+		/// </summary>
+		/// <param name="data">The input data.</param>
+		/// <returns>The result of the rule.</returns>
 		public abstract JsonElement Apply(JsonElement data);
 
+		/// <summary>
+		/// Casts a JSON value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(JsonElement value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts an `int` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(int value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `string` value to a <see cref="LiteralRule"/>.  Can also be used to create a `null` JSON literal.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(string value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `bool` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(bool value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `long` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(long value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `decimal` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(decimal value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `float` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(float value) => new LiteralRule(value);
+		/// <summary>
+		/// Casts a `double` value to a <see cref="LiteralRule"/>.
+		/// </summary>
+		/// <param name="value">The value.</param>
 		public static implicit operator Rule(double value) => new LiteralRule(value);
 	}
 
+	/// <summary>
+	/// Provides serialization for all <see cref="Rule"/> derivatives.
+	/// </summary>
 	public class LogicComponentConverter : JsonConverter<Rule>
 	{
+		/// <summary>Reads and converts the JSON to type <see cref="Rule"/>.</summary>
+		/// <param name="reader">The reader.</param>
+		/// <param name="typeToConvert">The type to convert.</param>
+		/// <param name="options">An object that specifies serialization options to use.</param>
+		/// <returns>The converted value.</returns>
 		public override Rule Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 		{
 			if (reader.TokenType != JsonTokenType.StartObject)
@@ -46,6 +94,10 @@ namespace Json.Logic
 					.ToArray());
 		}
 
+		/// <summary>Writes a specified value as JSON.</summary>
+		/// <param name="writer">The writer to write to.</param>
+		/// <param name="value">The value to convert to JSON.</param>
+		/// <param name="options">An object that specifies serialization options to use.</param>
 		public override void Write(Utf8JsonWriter writer, Rule value, JsonSerializerOptions options)
 		{
 			throw new NotImplementedException();
--- JsonLogic/RuleExtensions.cs
+++ JsonLogic/RuleExtensions.cs
@@ -2,11 +2,19 @@ using System.Text.Json;
 
 namespace Json.Logic
 {
+	/// <summary>
+	/// Calls <see cref="Rule.Apply(JsonElement)"/> with no data.
+	/// </summary>
 	public static class RuleExtensions
 	{
-		public static JsonElement Apply(this Rule component)
+		/// <summary>
+		/// Calls <see cref="Rule.Apply(JsonElement)"/> with no data.
+		/// </summary>
+		/// <param name="rule">The rule.</param>
+		/// <returns>The result.</returns>
+		public static JsonElement Apply(this Rule rule)
 		{
-			return component.Apply(JsonDocument.Parse("null").RootElement);
+			return rule.Apply(JsonDocument.Parse("null").RootElement);
 		}
 	}
 }
