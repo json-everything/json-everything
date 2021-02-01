3c00647a31cd15f7cf872e6b7081aa45160dd874
Greg Dennis <gregsdennis@yahoo.com>
2021-02-01 17:23:41 +1300

    keyword impl scaffold plus serialize

---
+++ JsonSchema.Data/DataKeyword.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Json.Schema.Data
+{
+	[SchemaKeyword(Name)]
+	[SchemaPriority(int.MinValue)]
+	[SchemaDraft(Draft.Draft201909)]
+	[SchemaDraft(Draft.Draft202012)]
+	[Vocabulary(Vocabularies.DataId)]
+	[JsonConverter(typeof(DataKeywordJsonConverter))]
+	public class DataKeyword : IJsonSchemaKeyword
+	{
+		internal const string Name = "data";
+
+		public IReadOnlyDictionary<string, Uri> References { get; }
+
+		public DataKeyword(IReadOnlyDictionary<string, Uri> references)
+		{
+			References = references;
+		}
+
+		public void Validate(ValidationContext context)
+		{
+			// resolve data
+			
+			// serialize values
+			
+			// deserialize as schema
+
+			// return evaluation
+
+			throw new NotImplementedException();
+		}
+	}
+
+	internal class DataKeywordJsonConverter : JsonConverter<DataKeyword>
+	{
+		public override DataKeyword Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			if (reader.TokenType != JsonTokenType.StartObject)
+				throw new JsonException("Expected object");
+
+			var references = JsonSerializer.Deserialize<Dictionary<string, string>>(ref reader, options)
+				.ToDictionary(kvp => kvp.Key, kvp => new Uri(kvp.Value));
+			return new DataKeyword(references);
+		}
+
+		public override void Write(Utf8JsonWriter writer, DataKeyword value, JsonSerializerOptions options)
+		{
+			writer.WritePropertyName(DataKeyword.Name);
+			writer.WriteStartObject();
+			foreach (var kvp in value.References)
+			{
+				writer.WritePropertyName(kvp.Key);
+				JsonSerializer.Serialize(writer, kvp.Value, options);
+			}
+			writer.WriteEndObject();
+		}
+	}
+}
---
+++ JsonSchema.Data/Vocabularies.cs
@@ -0,0 +1,9 @@
+﻿namespace Json.Schema.Data
+{
+	public static class Vocabularies
+	{
+		public const string DataId = "https://gregsdennis.github.io/json-everything/vocabs-data";
+
+		public static readonly Vocabulary Data = new Vocabulary(DataId, typeof(DataKeyword));
+	}
+}
