fa237202186177c94a7b648ccd36ff22ad0c4375
Greg Dennis <gregsdennis@yahoo.com>
2021-04-24 19:26:13 +1200

    added test project for more lib and tests for enum converter

---
+++ Json.More.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NUnit.Framework;
+
+namespace Json.More.Tests
+{
+	public class EnumStringConverterTests
+	{
+		class ConversionTest
+		{
+			[JsonConverter(typeof(EnumStringConverter<DayOfWeek>))]
+			public DayOfWeek Day { get; set; }
+		}
+
+		[Test]
+		public void DayOfWeekAsPropertyIsConverted()
+		{
+			var expected = "{\"Day\":\"Wednesday\"}";
+			var actual = JsonSerializer.Serialize(new ConversionTest {Day = DayOfWeek.Wednesday});
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[JsonConverter(typeof(EnumStringConverter<CustomEnum>))]
+		public enum CustomEnum
+		{
+			Zero,
+			[System.ComponentModel.Description("one")]
+			One,
+			Two
+		}
+
+		[TestCase(CustomEnum.Zero, "Zero")]
+		[TestCase(CustomEnum.One, "one")]
+		[TestCase(CustomEnum.Two, "Two")]
+		public void CustomEnumIsConverted(CustomEnum value, string serializedValue)
+		{
+			var expected = $"\"{serializedValue}\"";
+			var actual = JsonSerializer.Serialize(value);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
