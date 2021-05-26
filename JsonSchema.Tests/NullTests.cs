cd482daa587091f4da5264d2f9340a54ffccd184
Greg Dennis <gregsdennis@yahoo.com>
2021-05-26 21:24:28 +1200

    added null tests for schema; no functional changes

---
+++ JsonSchema.Tests/NullTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using NUnit.Framework;
+
+namespace Json.Schema.Tests
+{
+	public class NullTests
+	{
+		private static readonly JsonSchema _schema =
+			new JsonSchemaBuilder()
+				.Type(SchemaValueType.Object)
+				.Required("foo");
+
+		[Test]
+		public void PropertyWithNullValuePasses()
+		{
+			var json = JsonDocument.Parse("{\"foo\": null}").RootElement;
+
+			var result = _schema.Validate(json, new ValidationOptions {OutputFormat = OutputFormat.Detailed});
+
+			result.AssertValid();
+		}
+
+		[Test]
+		public void MissingPropertyFails()
+		{
+			var json = JsonDocument.Parse("{}").RootElement;
+
+			var result = _schema.Validate(json, new ValidationOptions { OutputFormat = OutputFormat.Detailed });
+
+			result.AssertInvalid();
+		}
+	}
+}
