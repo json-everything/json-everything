f7859e6a184b6546a31b321eef8ec07075c10181
Greg Dennis <gregsdennis@yahoo.com>
2021-06-05 19:12:31 +1200

    scaffolding for uniqueKeys impl

---
+++ JsonSchema.UniqueKeys.Tests/Tests.cs
@@ -0,0 +1,10 @@
+using System.Text.Json;
+using Json.Schema.Tests;
+using NUnit.Framework;
+
+namespace Json.Schema.UniqueKeys.Tests
+{
+	public class Tests
+	{
+	}
+}
---
+++ JsonSchema.UniqueKeys/JsonSchemaBuilderExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Json.Pointer;
+
+namespace Json.Schema.UniqueKeys
+{
+	/// <summary>
+	/// Provides a fluent interface for <see cref="JsonSchemaBuilder"/>.
+	/// </summary>
+	public static class JsonSchemaBuilderExtensions
+	{
+		/// <summary>
+		/// Adds a `uniqueKeys` keyword.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="keys">The collection of pointers to the keys which should be unique within the array.</param>
+		/// <returns>The builder.</returns>
+		public static JsonSchemaBuilder UniqueKeys(this JsonSchemaBuilder builder, IEnumerable<JsonPointer> keys)
+		{
+			builder.Add(new UniqueKeysKeyword(keys));
+			return builder;
+		}
+
+		/// <summary>
+		/// Adds a `uniqueKeys` keyword.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="keys">The collection of pointers to the keys which should be unique within the array.</param>
+		/// <returns>The builder.</returns>
+		public static JsonSchemaBuilder UniqueKeys(this JsonSchemaBuilder builder, IEnumerable<string> keys)
+		{
+			builder.Add(new UniqueKeysKeyword(keys.Select(x => JsonPointer.Parse(x))));
+			return builder;
+		}
+
+		/// <summary>
+		/// Adds a `uniqueKeys` keyword.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="keys">The collection of pointers to the keys which should be unique within the array.</param>
+		/// <returns>The builder.</returns>
+		public static JsonSchemaBuilder UniqueKeys(this JsonSchemaBuilder builder, params JsonPointer[] keys)
+		{
+			builder.Add(new UniqueKeysKeyword(keys));
+			return builder;
+		}
+
+		/// <summary>
+		/// Adds a `uniqueKeys` keyword.
+		/// </summary>
+		/// <param name="builder">The builder.</param>
+		/// <param name="keys">The collection of pointers to the keys which should be unique within the array.</param>
+		/// <returns>The builder.</returns>
+		public static JsonSchemaBuilder UniqueKeys(this JsonSchemaBuilder builder, params string[] keys)
+		{
+			builder.Add(new UniqueKeysKeyword(keys.Select(x => JsonPointer.Parse(x))));
+			return builder;
+		}
+	}
+}
