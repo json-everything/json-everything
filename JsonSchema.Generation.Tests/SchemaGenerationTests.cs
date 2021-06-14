c16a4739c50fa9b59c81af4639edee02c79a5ee5
JMPSequeira <email.sequeira@gmail.com>
2021-06-14 23:02:22 +0100

    add tests

--- JsonSchema.Generation.Tests/SchemaGenerationTests.cs
+++ JsonSchema.Generation.Tests/SchemaGenerationTests.cs
@@ -11,8 +11,7 @@ namespace Json.Schema.Generation.Tests
 	{
 		public static IEnumerable<TestCaseData> SimpleTypeCases
 		{
-			get
-			{
+			get {
 				yield return new TestCaseData(typeof(bool), SchemaValueType.Boolean);
 				yield return new TestCaseData(typeof(string), SchemaValueType.String);
 				yield return new TestCaseData(typeof(byte), SchemaValueType.Integer);
@@ -25,6 +24,17 @@ namespace Json.Schema.Generation.Tests
 				yield return new TestCaseData(typeof(float), SchemaValueType.Number);
 				yield return new TestCaseData(typeof(double), SchemaValueType.Number);
 				yield return new TestCaseData(typeof(decimal), SchemaValueType.Number);
+				yield return new TestCaseData(typeof(bool?), SchemaValueType.Boolean);
+				yield return new TestCaseData(typeof(byte?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(short?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(ushort?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(int?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(uint?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(long?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(ulong?), SchemaValueType.Integer);
+				yield return new TestCaseData(typeof(float?), SchemaValueType.Number);
+				yield return new TestCaseData(typeof(double?), SchemaValueType.Number);
+				yield return new TestCaseData(typeof(decimal?), SchemaValueType.Number);
 			}
 		}
 
@@ -45,6 +55,17 @@ namespace Json.Schema.Generation.Tests
 				.Items(new JsonSchemaBuilder().Type(SchemaValueType.Integer));
 
 			JsonSchema actual = new JsonSchemaBuilder().FromType<int[]>();
+
+			AssertEqual(expected, actual);
+		}
+
+		[Test]
+		public void NullableIntArray()
+		{
+			JsonSchema expected = new JsonSchemaBuilder().Type(SchemaValueType.Array)
+				.Items(new JsonSchemaBuilder().Type(SchemaValueType.Integer));
+
+			JsonSchema actual = new JsonSchemaBuilder().FromType<int?[]>();
 
 			AssertEqual(expected, actual);
 		}
