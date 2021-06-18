0f69656a52b541ef28da0064b91d478c4f8ab4ad
Greg Dennis <gregsdennis@yahoo.com>
2021-06-18 18:42:09 +1200

    all tests pass

--- JsonSchema.Generation/Refiners/NullabilityRefiner.cs
+++ JsonSchema.Generation/Refiners/NullabilityRefiner.cs
@@ -1,33 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Json.Schema.Generation.Intents;
 
 namespace Json.Schema.Generation.Refiners
 {
-	/// <summary>
-	/// Refines schemas
-	/// </summary>
-	public class NullabilityRefiner : ISchemaRefiner
+	internal class NullabilityRefiner : ISchemaRefiner
 	{
 		public static NullabilityRefiner Instance { get; } = new NullabilityRefiner();
 
 		private NullabilityRefiner(){}
 
-		/// <summary>
-		/// Determines if the refiner should run.
-		/// </summary>
-		/// <param name="context"></param>
-		/// <returns></returns>
 		public bool ShouldRun(SchemaGeneratorContext context)
 		{
 			return context.Intents.OfType<TypeIntent>().Any();
 		}
 
-		/// <summary>
-		/// Runs the refiner.
-		/// </summary>
-		/// <param name="context"></param>
 		public void Run(SchemaGeneratorContext context)
 		{
 			var typeIntent = context.Intents.OfType<TypeIntent>().FirstOrDefault();
@@ -50,7 +37,8 @@ namespace Json.Schema.Generation.Refiners
 				typeIntent.Type |= SchemaValueType.Null;
 
 			if (context.Configuration.Nullability.HasFlag(Nullability.AllowForReferenceTypes) &&
-			    context.Type.IsByRef)
+				// see https://stackoverflow.com/a/16578846/878701
+				!context.Type.IsValueType)
 				typeIntent.Type |= SchemaValueType.Null;
 		}
 	}
