27357f56a87870bfd13229ac702dc96c84565521
Greg Dennis <gregsdennis@yahoo.com>
2021-06-18 19:57:05 +1200

    nullable generator is really just an unwrapper

--- JsonSchema.Generation/Generators/NullableValueTypeSchemaGenerator.cs
+++ JsonSchema.Generation/Generators/NullableValueTypeSchemaGenerator.cs
@@ -1,5 +1,4 @@
-﻿using Json.Schema.Generation.Intents;
-using System;
+﻿using System;
 
 namespace Json.Schema.Generation.Generators
 {
@@ -7,44 +6,17 @@ namespace Json.Schema.Generation.Generators
 	{
 		public bool Handles(Type type)
 		{
-			var underlyingType = Nullable.GetUnderlyingType(type);
-
-			if (underlyingType != null)
-			{
-				return GeneratorRegistry.Get(underlyingType) != null;
-			}
-
-			return false;
+			return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
 		}
 
 		public void AddConstraints(SchemaGeneratorContext context)
 		{
 			var underlyingType = Nullable.GetUnderlyingType(context.Type);
 
-			if (underlyingType != null)
-			{
-				var underlyingContext = new SchemaGeneratorContext(underlyingType, context.Attributes, context.Configuration);
+			if (underlyingType == null) return;
+			var underlyingContext = SchemaGenerationContextCache.Get(underlyingType, context.Attributes, context.Configuration);
 
-				GeneratorRegistry.Get(Nullable.GetUnderlyingType(context.Type))?
-					.AddConstraints(underlyingContext);
-
-
-				var nullable = (context.Configuration.Nullability & Nullability.AllowForNullableValueTypes)
-					== Nullability.AllowForNullableValueTypes;
-
-				foreach (var intent in underlyingContext.Intents)
-				{
-					if (nullable
-						&& intent is TypeIntent tIntent
-						&& (tIntent.Type & SchemaValueType.Null) == 0)
-					{
-						context.Intents.Add(new TypeIntent(tIntent.Type | SchemaValueType.Null));
-						continue;
-					}
-
-					context.Intents.Add(intent);
-				}
-			}
+			context.Intents.AddRange(underlyingContext.Intents);
 		}
 	}
 }
