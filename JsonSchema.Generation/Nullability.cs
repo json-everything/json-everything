cdcc43c1d3c8b21800b5b6ec82502ef263e6f3d8
Greg Dennis <gregsdennis@yahoo.com>
2021-06-18 18:15:08 +1200

    added nullability refiner; copied nullability config from #127

---
+++ JsonSchema.Generation/Nullability.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Json.Schema.Generation
+{
+	/// <summary>
+	/// Indicates whether to apply `null` to the `type` keyword.
+	/// </summary>
+	[Flags]
+	public enum Nullability
+	{
+		/// <summary>
+		/// Indicates that `null` will not be included.
+		/// </summary>
+		Disabled = 0,
+		/// <summary>
+		/// Indicates that `null` will be applied to <see cref="Nullable{T}"/> types.
+		/// </summary>
+		AllowForNullableValueTypes = 1 << 0,
+		/// <summary>
+		/// Indicates that `null` will be applied to reference types.
+		/// </summary>
+		AllowForReferenceTypes = 1 << 1,
+		/// <summary>
+		/// Indicates that `null` will be applied to both reference types and <see cref="Nullable{T}"/> types.
+		/// </summary>
+		AllowForAllTypes = AllowForNullableValueTypes | AllowForReferenceTypes
+	}
+}
---
+++ JsonSchema.Generation/Refiners/NullabilityRefiner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Json.Schema.Generation.Intents;
+
+namespace Json.Schema.Generation.Refiners
+{
+	/// <summary>
+	/// Refines schemas
+	/// </summary>
+	public class NullabilityRefiner : ISchemaRefiner
+	{
+		public static NullabilityRefiner Instance { get; } = new NullabilityRefiner();
+
+		private NullabilityRefiner(){}
+
+		/// <summary>
+		/// Determines if the refiner should run.
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		public bool ShouldRun(SchemaGeneratorContext context)
+		{
+			return context.Intents.OfType<TypeIntent>().Any();
+		}
+
+		/// <summary>
+		/// Runs the refiner.
+		/// </summary>
+		/// <param name="context"></param>
+		public void Run(SchemaGeneratorContext context)
+		{
+			var typeIntent = context.Intents.OfType<TypeIntent>().FirstOrDefault();
+			if (typeIntent == null) return; // shouldn't happen because of ShouldRun(), but including just in case.
+
+			var nullableAttribute = context.Attributes.OfType<NullableAttribute>().FirstOrDefault();
+			var nullabilityOverride = nullableAttribute?.IsNullable;
+
+			if (context.Configuration.Nullability.HasFlag(Nullability.AllowForNullableValueTypes) &&
+			    context.Type.IsGenericType && context.Type.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+			    nullabilityOverride == true)
+				typeIntent.Type |= SchemaValueType.Null;
+
+			if (context.Configuration.Nullability.HasFlag(Nullability.AllowForReferenceTypes) &&
+			    context.Type.IsByRef)
+				typeIntent.Type |= SchemaValueType.Null;
+		}
+	}
+}
