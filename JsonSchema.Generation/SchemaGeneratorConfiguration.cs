2d830d5a88dae02f90bdcbf67bd2d7007fb81e2d
Greg Dennis <gregsdennis@yahoo.com>
2021-06-29 20:09:18 +1200

    added configuration property and several out-of-the-box naming conventions

--- JsonSchema.Generation/SchemaGeneratorConfiguration.cs
+++ JsonSchema.Generation/SchemaGeneratorConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using Json.Schema.Generation.Generators;
-using Json.Schema.Generation.Refiners;
 
 namespace Json.Schema.Generation
 {
@@ -9,6 +8,8 @@ namespace Json.Schema.Generation
 	/// </summary>
 	public class SchemaGeneratorConfiguration
 	{
+		private PropertyNamingMethod? _propertyNamingMethod;
+
 		/// <summary>
 		/// A collection of refiners.
 		/// </summary>
@@ -21,6 +22,17 @@ namespace Json.Schema.Generation
 		/// Gets or sets the order in which properties will be listed in the schema.
 		/// </summary>
 		public PropertyOrder PropertyOrder { get; set; }
+		/// <summary>
+		/// Gets or sets the property naming method.  Default is <see cref="PropertyNamingMethod.AsDeclared"/>.
+		/// </summary>
+		/// <remarks>
+		/// This can be replaced with any `Func&lt;string, string&gt;`.
+		/// </remarks>
+		public PropertyNamingMethod PropertyNamingMethod
+		{
+			get => _propertyNamingMethod ??= PropertyNamingMethods.AsDeclared;
+			set => _propertyNamingMethod = value;
+		}
 		/// <summary>
 		/// Gets or sets whether to include `null` in the `type` keyword.
 		/// Default is <see cref="Nullability.Disabled"/> which means that it will
