6cc8120de1cc1fed9ee901aab86e5d7ea173e610
Greg Dennis <gregsdennis@yahoo.com>
2021-08-22 15:50:16 +1200

    added code editors to schema page

---
+++ TryJsonEverything/wwwroot/js/schema.js
@@ -0,0 +1,13 @@
+﻿function initializeEditor(name) {
+	var editor = ace.edit(name);
+	editor.setTheme("ace/theme/monokai");
+	editor.session.setMode("ace/mode/json");
+	editor.session.setOptions({
+		theme: "ace/theme/monokai",
+		mode: "ace/mode/json",
+		tabSize: 2
+	});
+}
+
+initializeEditor("editor-schema");
+initializeEditor("editor-instance");
