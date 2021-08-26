7a40d9676ccb2ef8125c74fa760eaa1231c46096
Greg Dennis <gregsdennis@yahoo.com>
2021-08-27 00:28:00 +1200

    updated to run online

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ RUN dotnet publish "TryJsonEverything.csproj" -c Release -o /app/publish
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
-ENTRYPOINT ["dotnet", "TryJsonEverything.dll"]
+CMD ["dotnet", "TryJsonEverything.dll"]
--- TryJsonEverything/Program.cs
+++ TryJsonEverything/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace TryJsonEverything
@@ -20,7 +21,10 @@ namespace TryJsonEverything
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.UseStartup<Startup>();
+					webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.Listen(IPAddress.Any, Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
+                    }).UseStartup<Startup>();
 				});
 	}
 }
