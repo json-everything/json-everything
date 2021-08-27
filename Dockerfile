c69c152c84407e634342b5dd1e7029dff763ce1d
Greg Dennis <gregsdennis@yahoo.com>
2021-08-27 17:04:43 +1200

    add env var to dockerfile; run from vs; add action

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
+ENV ASPNETCORE_ENVIRONMENT=Production
 
 FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
 WORKDIR /src
--- TryJsonEverything/Program.cs
+++ TryJsonEverything/Program.cs
@@ -1,12 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace TryJsonEverything
 {
@@ -21,10 +16,12 @@ namespace TryJsonEverything
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.ConfigureKestrel(serverOptions =>
-                    {
-                        serverOptions.Listen(IPAddress.Any, Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
-                    }).UseStartup<Startup>();
+					if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Development")
+						webBuilder.ConfigureKestrel(serverOptions =>
+						{
+							serverOptions.Listen(IPAddress.Any, Convert.ToInt32(Environment.GetEnvironmentVariable("PORT")));
+						});
+					webBuilder.UseStartup<Startup>();
 				});
 	}
 }
