dd1fb5cf23e2433c31b0ef9de809a2d60e59a431
Greg Dennis <gregsdennis@yahoo.com>
2021-09-06 23:13:23 +1200

    better stringifying for paths

--- JsonPath.Tests/OtherParsingTests.cs
+++ JsonPath.Tests/OtherParsingTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Json.Path.Tests
@@ -10,13 +11,16 @@ namespace Json.Path.Tests
 			{
 				new TestCaseData("$.baz"),
 				new TestCaseData("$[?(@.foo==(4+5))]"),
+				new TestCaseData("$[?(@.foo==2*(4+5))]"),
+				new TestCaseData("$[?(@.foo==2+(4+5))]"),
+				new TestCaseData("$[?(@.foo==2-(4+5))]"),
 				new TestCaseData("$[?(@['name'] == null || @['name'] == 'abc')]"),
 			};
 
 		[TestCaseSource(nameof(SuccessCases))]
 		public void ParseSingleProperty(string path)
 		{
-			JsonPath.Parse(path);
+			Console.WriteLine(JsonPath.Parse(path));
 		}
 	}
 }
