de64785b961d9aa97928ae538ef590d72d7cf561
Greg Dennis <gregsdennis@yahoo.com>
2021-11-15 19:15:16 +1300

    added tests to reproduce #183

---
+++ JsonLogic.Tests/GithubTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Text.Json;
+using NUnit.Framework;
+
+namespace Json.Logic.Tests
+{
+	public class GithubTests
+	{
+		[Test]
+		public void Issue183_RuleEvaluatesWrong_Truthy()
+		{
+			var logic = JsonLogic.If(
+				JsonLogic.Variable("data.sub"),
+				JsonLogic.StrictEquals(
+					JsonLogic.Variable("data.sub.0.element"),
+					"12345"
+				),
+				true
+			);
+			var json = JsonDocument.Parse(@"{
+   ""data"": {
+    ""sub"": [
+      {
+        ""element"": ""12345""
+      }
+    ]
+  }
+}").RootElement;
+
+			var result = logic.Apply(json);
+
+			JsonAssert.AreEquivalent(true, result);
+		}
+		[Test]
+		public void Issue183_RuleEvaluatesWrong_Falsy()
+		{
+			var logic = JsonLogic.If(
+				JsonLogic.Variable("data.sub"),
+				JsonLogic.StrictEquals(
+					JsonLogic.Variable("data.sub.0.element"),
+					"12345"
+				),
+				true
+			);
+			var json = JsonDocument.Parse(@"{
+   ""data"": {
+    ""sub"": [
+      {
+        ""element"": ""12346""
+      }
+    ]
+  }
+}").RootElement;
+
+			var result = logic.Apply(json);
+
+			JsonAssert.AreEquivalent(false, result);
+		}
+	}
+}
