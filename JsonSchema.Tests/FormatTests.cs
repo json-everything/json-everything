1be332ab664d8ebcf28d43cd05b50c5e796a8dd6
Greg Dennis <gregsdennis@yahoo.com>
2021-12-05 12:02:45 +1300

    added tests to replicate

--- JsonSchema.Tests/FormatTests.cs
+++ JsonSchema.Tests/FormatTests.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Text.Json;
+using Json.More;
 using NUnit.Framework;
 
 namespace Json.Schema.Tests
@@ -28,6 +31,82 @@ namespace Json.Schema.Tests
 			var result = schema.Validate(value.RootElement, new ValidationOptions {RequireFormatValidation = true});
 
 			Assert.False(result.IsValid);
+		}
+
+		private static Uri FormatAssertionMetaSchemaId = new Uri("https://json-everything/test/format-assertion");
+		private static JsonSchema FormatAssertionMetaSchema =
+						new JsonSchemaBuilder()
+				.Schema(MetaSchemas.Draft202012Id)
+				.Id(FormatAssertionMetaSchemaId)
+				.Vocabulary(
+					(Vocabularies.Core202012Id, true),
+					(Vocabularies.Applicator202012Id, true),
+					(Vocabularies.Metadata202012Id, true),
+					(Vocabularies.FormatAnnotation202012Id, false)
+				)
+				.DynamicAnchor("meta")
+				.Title("format assertion meta-schema")
+				.AllOf(
+					new JsonSchemaBuilder().Ref(MetaSchemas.Core202012Id),
+					new JsonSchemaBuilder().Ref(MetaSchemas.Applicator202012Id),
+					new JsonSchemaBuilder().Ref(MetaSchemas.Metadata202012Id),
+					new JsonSchemaBuilder().Ref(MetaSchemas.FormatAssertion202012Id)
+				)
+				.Type(SchemaValueType.Object | SchemaValueType.Boolean);
+
+		[Test]
+		public void UnknownFormat_Annotation_ReportsFormat()
+		{
+			var schemaText = $@"{{
+	""$schema"": ""{MetaSchemas.Draft202012Id}"",
+	""type"": ""string"",
+	""format"": ""something-dumb""
+}}";
+			var schema = JsonSchema.FromText(schemaText);
+			var instance = JsonDocument.Parse("\"a value\"").RootElement;
+
+			var results = schema.Validate(instance, new ValidationOptions{OutputFormat = OutputFormat.Detailed});
+
+			results.AssertValid();
+		}
+
+		[Test]
+		public void UnknownFormat_Assertion_FailsValidation()
+		{
+			var options = new ValidationOptions {OutputFormat = OutputFormat.Detailed};
+			options.SchemaRegistry.Register(FormatAssertionMetaSchemaId, FormatAssertionMetaSchema);
+
+			var schemaText = $@"{{
+	""$schema"": ""{FormatAssertionMetaSchemaId}"",
+	""type"": ""string"",
+	""format"": ""something-dumb""
+}}";
+			var schema = JsonSchema.FromText(schemaText);
+			var instance = JsonDocument.Parse("\"a value\"").RootElement;
+
+			var results = schema.Validate(instance, options);
+
+			results.AssertInvalid();
+		}
+
+		[Test]
+		public void UnknownFormat_AnnotationWithAssertionOption_FailsValidation()
+		{
+			var schemaText = $@"{{
+	""$schema"": ""{MetaSchemas.Draft202012Id}"",
+	""type"": ""string"",
+	""format"": ""something-dumb""
+}}";
+			var schema = JsonSchema.FromText(schemaText);
+			var instance = JsonDocument.Parse("\"a value\"").RootElement;
+
+			var results = schema.Validate(instance, new ValidationOptions
+			{
+				OutputFormat = OutputFormat.Detailed,
+				RequireFormatValidation = true
+			});
+
+			results.AssertInvalid();
 		}
 	}
 }
