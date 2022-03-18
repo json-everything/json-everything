26b5fa26550af2e021312eb81ad003672fbe991c
Greg Dennis <gregsdennis@yahoo.com>
2022-03-19 11:38:23 +1300

    blazor works! also some styling

---
+++ json-everything.net/Services/LibraryVersion.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+#pragma warning disable 8618
+
+namespace JsonEverythingNet.Services;
+
+public class LibraryVersion
+{
+	public string Name { get; set; }
+	public string Version { get; set; }
+
+	public static LibraryVersion GetFor<T>()
+	{
+		var attribute = typeof(T).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+		var version = Regex.Match(attribute!.InformationalVersion, @"\d+\.\d+\.\d+").Value;
+		return new LibraryVersion
+		{
+			Name = typeof(T).Assembly.GetName().Name!,
+			Version = version
+		};
+	}
+}
