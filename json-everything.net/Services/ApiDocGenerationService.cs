9895ea133f03ea23bc54b5441bdf4b40b9b5ec0b
Greg Dennis <gregsdennis@yahoo.com>
2022-06-14 13:59:41 +1200

    finalizing toc and adding to all pages

---
+++ json-everything.net/Services/ApiDocGenerationService.cs
@@ -0,0 +1,9 @@
+﻿namespace JsonEverythingNet.Services;
+
+public class ApiDocGenerationService
+{
+	public string GenerateForType(Type type)
+	{
+		return $"# Look at me!\n\nI'm `{type.CSharpName()}` API docs!";
+	}
+}
