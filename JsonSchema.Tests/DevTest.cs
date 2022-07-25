f4f8e663f1f89c9732f2a886e577c93cf19e3f47
Greg Dennis <gregsdennis@yahoo.com>
2022-07-25 13:19:03 +1200

    fix image file path

--- JsonSchema.Tests/DevTest.cs
+++ JsonSchema.Tests/DevTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using NUnit.Framework;
@@ -10,10 +11,29 @@ public class DevTest
 	[Test]
 	public void Test()
 	{
-		var node = JsonNode.Parse("1238723762349702529873649378023892834969761287612402596");
+		JsonSchema schema = new JsonSchemaBuilder()
+			.Schema(MetaSchemas.Draft202012Id)
+			.Id("example-schema")
+			.Type(SchemaValueType.Object)
+			.Title("foo object schema")
+			.Properties(
+				("foo", new JsonSchemaBuilder()
+					.Title("foo's title")
+					.Description("foo's description")
+					.Type(SchemaValueType.String)
+				)
+			)
+			.Required("foo")
+			.AdditionalProperties(false);
 
-		var value = node.GetValue<object>();
+		var instance = new JsonObject { ["foo"] = new JsonObject { ["definitely"] = "not a string" } };
 
-		Console.WriteLine(value.GetType());
+		var results = schema.Validate(instance, new ValidationOptions { OutputFormat = OutputFormat.Basic });
+
+		Console.WriteLine(JsonSerializer.Serialize(results, new JsonSerializerOptions
+		{
+			WriteIndented = true,
+			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+		}));
 	}
 }
