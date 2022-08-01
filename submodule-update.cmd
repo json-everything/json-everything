23dae3ad2d65235d0e3692b162f6a86a8aa22d71
Greg Dennis <gregsdennis@yahoo.com>
2022-08-02 10:53:00 +1200

    updated test suites; added script to do so

---
+++ submodule-update.cmd
@@ -0,0 +1,27 @@
+cd ref-repos
+
+cd json-patch-tests
+git fetch
+git checkout master
+git pull
+cd ..
+
+cd json-path-comparison
+git fetch
+git checkout master
+git pull
+cd ..
+
+cd JSON-Schema-Test-Suite
+git fetch
+git checkout main
+git pull
+cd ..
+
+cd jsonpath-compliance-test-suite
+git fetch
+git checkout main
+git pull
+cd ..
+
+cd ..
