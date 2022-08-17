dad15dcea98e989b55f58cea36e46af458b3e59f
Greg Dennis <gregsdennis@yahoo.com>
2022-08-18 10:34:22 +1200

    initial scaffolding for propertyDependencies keyword

---
+++ JsonSchema/PropertyDependenciesKeyword.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Json.Schema;
+
+[Applicator]
+[SchemaKeyword(Name)]
+[JsonConverter(typeof(PropertyDependenciesKeywordJsonConverter))]
+public class PropertyDependenciesKeyword : IJsonSchemaKeyword, IEquatable<PropertyDependenciesKeyword>
+{
+	internal const string Name = "propertyDependencies";
+
+	public IReadOnlyDictionary<string, PropertyDependency> Dependencies { get; }
+
+	public PropertyDependenciesKeyword(IReadOnlyDictionary<string, PropertyDependency> dependencies)
+	{
+		Dependencies = dependencies;
+	}
+
+	public void Validate(ValidationContext context)
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool Equals(PropertyDependenciesKeyword? other)
+	{
+		if (ReferenceEquals(null, other)) return false;
+		if (ReferenceEquals(this, other)) return true;
+		return Dependencies.Equals(other.Dependencies);
+	}
+
+	public override bool Equals(object? obj)
+	{
+		return Equals(obj as PropertyDependenciesKeyword);
+	}
+
+	public override int GetHashCode()
+	{
+		return Dependencies.GetStringDictionaryHashCode();
+	}
+}
+
+internal class PropertyDependenciesKeywordJsonConverter : JsonConverter<PropertyDependenciesKeyword>
+{
+	public override PropertyDependenciesKeyword? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(Utf8JsonWriter writer, PropertyDependenciesKeyword value, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ JsonSchema/PropertyDependency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Json.Schema;
+
+[JsonConverter(typeof(PropertyDependencyJsonConverter))]
+public class PropertyDependency : IKeyedSchemaCollector, IEquatable<PropertyDependency>
+{
+	public IReadOnlyDictionary<string, JsonSchema> Schemas { get; }
+
+	public PropertyDependency(IReadOnlyDictionary<string, JsonSchema> schemas)
+	{
+		Schemas = schemas;
+	}
+
+	public bool Equals(PropertyDependency? other)
+	{
+		if (ReferenceEquals(null, other)) return false;
+		if (ReferenceEquals(this, other)) return true;
+		return Schemas.Equals(other.Schemas);
+	}
+
+	public override bool Equals(object? obj)
+	{
+		return Equals(obj as PropertyDependency);
+	}
+
+	public override int GetHashCode()
+	{
+		return Schemas.GetStringDictionaryHashCode();
+	}
+}
+
+internal class PropertyDependencyJsonConverter : JsonConverter<PropertyDependency>
+{
+	public override PropertyDependency? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(Utf8JsonWriter writer, PropertyDependency value, JsonSerializerOptions options)
+	{
+		throw new NotImplementedException();
+	}
+}
