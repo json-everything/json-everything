54f845622c50bddc1b9a0a5156de37828c7bf813
Greg Dennis <gregsdennis@yahoo.com>
2022-08-29 11:40:55 +1200

    update unsupported case check

--- JsonSchema.Tests/Suite/Suite.cs
+++ JsonSchema.Tests/Suite/Suite.cs
@@ -16,8 +16,8 @@ public class Suite
 	private const string _testCasesPath = @"../../../../ref-repos/JSON-Schema-Test-Suite/tests";
 	private const string _remoteSchemasPath = @"../../../../ref-repos/JSON-Schema-Test-Suite/remotes";
 
-	private const bool _useExternal = false;
-	private const bool _runDraftNext = false;
+	private const bool _useExternal = true;
+	private const bool _runDraftNext = true;
 	private const string _externalTestCasesPath = @"../../../../../JSON-Schema-Test-Suite/tests";
 	private const string _externalRemoteSchemasPath = @"../../../../../JSON-Schema-Test-Suite/remotes";
 
