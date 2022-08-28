6988bd61c4f93b98b8e4daf184f7d94af5520b46
Greg Dennis <gregsdennis@yahoo.com>
2022-08-29 09:55:39 +1200

    update for draft-next

--- JsonSchema/PropertyDependency.cs
+++ JsonSchema/PropertyDependency.cs
@@ -6,16 +6,29 @@ using System.Text.Json.Serialization;
 
 namespace Json.Schema;
 
+/// <summary>
+/// Represents a single entry in the `propertyDependencies` keyword.
+/// </summary>
 [JsonConverter(typeof(PropertyDependencyJsonConverter))]
 public class PropertyDependency : IKeyedSchemaCollector, IEquatable<PropertyDependency>
 {
+	/// <summary>
+	/// Gets the collection of value-dependent schemas for this property.
+	/// </summary>
 	public IReadOnlyDictionary<string, JsonSchema> Schemas { get; }
 
+	/// <summary>
+	/// Creates a new instance of <see cref="PropertyDependency"/>.
+	/// </summary>
+	/// <param name="schemas">The collection of value-dependent schemas for this property</param>
 	public PropertyDependency(IReadOnlyDictionary<string, JsonSchema> schemas)
 	{
 		Schemas = schemas;
 	}
 
+	/// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+	/// <param name="other">An object to compare with this object.</param>
+	/// <returns>true if the current object is equal to the <paramref name="other">other</paramref> parameter; otherwise, false.</returns>
 	public bool Equals(PropertyDependency? other)
 	{
 		if (ReferenceEquals(null, other)) return false;
@@ -31,11 +44,16 @@ public class PropertyDependency : IKeyedSchemaCollector, IEquatable<PropertyDepe
 		return byKey.All(g => Equals(g.ThisDef, g.OtherDef));
 	}
 
+	/// <summary>Determines whether the specified object is equal to the current object.</summary>
+	/// <param name="obj">The object to compare with the current object.</param>
+	/// <returns>true if the specified object  is equal to the current object; otherwise, false.</returns>
 	public override bool Equals(object? obj)
 	{
 		return Equals(obj as PropertyDependency);
 	}
 
+	/// <summary>Serves as the default hash function.</summary>
+	/// <returns>A hash code for the current object.</returns>
 	public override int GetHashCode()
 	{
 		return Schemas.GetStringDictionaryHashCode();
