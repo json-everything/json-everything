244d4ae499cd897e4aaf91c7cb91c4b11e62eca0
Greg Dennis <gregsdennis@yahoo.com>
2022-12-16 12:26:25 +1300

    rudimentary test suite

---
+++ Yaml2JsonNode.Tests/PathExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Yaml2JsonNode.Tests;
+
+public static class PathExtensions
+{
+	public static string AdjustForPlatform(this string path)
+	{
+		return Environment.OSVersion.Platform == PlatformID.MacOSX ||
+			   Environment.OSVersion.Platform == PlatformID.Unix
+			? path.Replace("\\", "/")
+			: path.Replace("/", "\\");
+	}
+}
