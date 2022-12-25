71e783f957aa94f5e09e18e411a79aa0dbcd0289
Greg Dennis <gregsdennis@yahoo.com>
2022-12-25 16:22:41 +1300

    edit an xml comment

--- JsonPath/ISelector.cs
+++ JsonPath/ISelector.cs
@@ -13,13 +13,17 @@ public interface ISelector
 	/// Evaluates the selector.
 	/// </summary>
 	/// <param name="node">The node to evaluate.</param>
-	/// <param name="rootNode">The root node (typically used by filter selectors, e.g. `$[?@foo < $.bar]` </param>
+	/// <param name="rootNode">The root node (typically used by filter selectors, e.g. `$[?@foo &lt; $.bar]`)</param>
 	/// <returns>
 	/// A collection of nodes.
 	///
-	/// Semantically, this is a nodelist, but leaving as `IEnumerable<Node>` allows for deferred execution.
+	/// Semantically, this is a nodelist, but leaving as IEnumerable&lt;Node&gt; allows for deferred execution.
 	/// </returns>
 	IEnumerable<Node> Evaluate(Node node, JsonNode? rootNode);
 
+	/// <summary>
+	/// Builds a string using a string builder.
+	/// </summary>
+	/// <param name="builder">The string builder.</param>
 	void BuildString(StringBuilder builder);
 }
