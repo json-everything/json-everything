7379d2aa6491f588443bb36bb58f4a637a8ec877
Greg Dennis <gregsdennis@yahoo.com>
2023-03-10 08:41:22 +1300

    need a code change to run the build

--- JsonSchema.Tests/DevTest.cs
+++ JsonSchema.Tests/DevTest.cs
@@ -14,29 +14,12 @@ public class DevTest
 		JsonSchema schema = new JsonSchemaBuilder()
 			.Schema(MetaSchemas.Draft202012Id)
 			.Id("example-schema")
-			.Type(SchemaValueType.Object)
-			.Title("foo object schema")
-			.Properties(
-				("foo", new JsonSchemaBuilder()
-					.Title("foo's title")
-					.Description("foo's description")
-					.Type(SchemaValueType.String)
-					.Pattern("^foo ")
-					.MinLength(10)
-				)
-			)
-			.Required("foo")
-			.AdditionalProperties(false);
+			.Type(SchemaValueType.Object);
 
-		var instance = new JsonObject { ["foo"] = "foo awe;ovinawe" };
+		var instance = new JsonObject { ["foo"] = "test" };
 
-		var results = schema.Evaluate(instance, new EvaluationOptions { OutputFormat = OutputFormat.Hierarchical });
+		var results = schema.Evaluate(instance);
 
-		Console.WriteLine(JsonSerializer.Serialize(results, new JsonSerializerOptions
-		{
-			WriteIndented = true,
-			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-			Converters = { new Pre202012EvaluationResultsJsonConverter() }
-		}));
+		results.AssertValid();
 	}
 }
