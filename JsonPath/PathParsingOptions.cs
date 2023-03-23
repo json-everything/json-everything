0a4307be5b85986cb9765ce6ed7a8e3c07363ed5
Greg Dennis <gregsdennis@yahoo.com>
2023-03-24 10:39:46 +1300

    add options for things i want to support

--- JsonPath/PathParsingOptions.cs
+++ JsonPath/PathParsingOptions.cs
@@ -6,11 +6,51 @@
 public class PathParsingOptions
 {
 	/// <summary>
-	/// Gets or sets whether mathematical operators will be allowed.
+	/// Gets or sets whether mathematical operators are allowed.
 	/// </summary>
 	/// <remarks>
 	/// These operators are an extension of the specification, so they
 	/// are disallowed by default.
 	/// </remarks>
 	public bool AllowMathOperations { get; set; }
+
+	/// <summary>
+	/// Gets or sets whether a JSON Path can start with the `@` symbol
+	/// instead of `$`.
+	/// </summary>
+	public bool AllowRelativePathStart { get; set; }
+
+	/// <summary>
+	/// Gets or sets whether JSON objects and arrays are permitted in
+	/// expression syntax.
+	/// </summary>
+	/// <remarks>
+	/// Per the specification, only numbers, strings, `null`, `true`,
+	/// and `false` are permitted.
+	/// </remarks>
+	public bool AllowJsonConstructs { get; set; }
+
+	/// <summary>
+	/// Gets or sets whether the JSON Path can contain leading and
+	/// trailing whitespace.
+	/// </summary>
+	/// <remarks>
+	/// Per the specification, such whitespace is not strictly part of
+	/// the syntax, so they are forbidden.  It is expected that such
+	/// whitespace will be removed before passing the string onto
+	/// parsing functionality.
+	/// </remarks>
+	public bool TolerateLeadingAndTrailingWhitespace { get; set; }
+
+	/// <summary>
+	/// Gets or sets whether the `in` operator is allowed.
+	/// </summary>
+	/// <remarks>
+	/// These operators are an extension of the specification, so they
+	/// are disallowed by default.
+	///
+	/// Also requires that <see cref="AllowJsonConstructs"/> be set to
+	/// `true`.
+	/// </remarks>
+	public bool AllowInOperator { get; set; }
 }
