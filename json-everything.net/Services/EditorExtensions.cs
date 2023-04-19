2bd274ccb5b63c7f07a1a4bd8dace6753ced4de2
Greg Dennis <gregsdennis@yahoo.com>
2023-04-20 09:20:25 +1200

    handle errors better online

---
+++ json-everything.net/Services/EditorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BlazorMonaco;
+
+namespace JsonEverythingNet.Services;
+
+public static class EditorExtensions
+{
+	public static void SetLanguage(this MonacoEditor editor, string language)
+	{
+		editor.GetModel()
+			.ContinueWith(x => MonacoEditorBase.SetModelLanguage(x.Result, language));
+	}
+	public static async Task SetLanguageAsync(this MonacoEditor editor, string language)
+	{
+		var model = await editor.GetModel();
+		await MonacoEditorBase.SetModelLanguage(model, language);
+	}
+}
