3f0ee35e654d2f97b56b1ddd672877cc1d592bcb
Greg Dennis <gregsdennis@yahoo.com>
2023-05-31 11:27:34 +1200

    overwrite release note files when copying

--- tools/ApiDocsGenerator/Program.cs
+++ tools/ApiDocsGenerator/Program.cs
@@ -60,7 +60,7 @@ namespace ApiDocsGenerator
 			foreach (FileInfo file in dir.GetFiles())
 			{
 				string targetFilePath = Path.Combine(destinationDir, file.Name);
-				file.CopyTo(targetFilePath);
+				file.CopyTo(targetFilePath, true);
 			}
 
 			// If recursive and copying subdirectories, recursively call this method
