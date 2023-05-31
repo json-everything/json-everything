aca3eb30f8b31d3e992a10943ef58d069e389c74
Greg Dennis <gregsdennis@yahoo.com>
2023-06-01 08:48:56 +1200

    clear dev test

--- JsonPath.Tests/DevTest.cs
+++ JsonPath.Tests/DevTest.cs
@@ -11,9 +11,9 @@ internal class DevTest
 	[Test]
 	public void Test()
 	{
-		var date = "2023-08-07";
-		var otherDate = "2023-05-01";
+		//var date = "2023-08-07";
+		//var otherDate = "2023-05-01";
 
-		Assert.AreEqual(1, string.Compare(date, otherDate, StringComparison.Ordinal));
+		//Assert.AreEqual(1, string.Compare(date, otherDate, StringComparison.Ordinal));
 	}
 }
