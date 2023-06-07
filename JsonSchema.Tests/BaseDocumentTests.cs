dabcde8101c49c65c49cb595bca28dde2a4762a4
Greg Dennis <gregsdennis@yahoo.com>
2023-06-08 09:13:54 +1200

    replicate issue with test

--- JsonSchema.Tests/BaseDocumentTests.cs
+++ JsonSchema.Tests/BaseDocumentTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Json.Schema.Tests
@@ -73,6 +74,44 @@ namespace Json.Schema.Tests
 			var result = subjectSchema.Evaluate(instance, options);
 
 			result.AssertInvalid();
+		}
+
+		[Test]
+		public void ReferencesFromWithinEmbeddedSchemas()
+		{
+			var json = new JsonObject
+			{
+				["prop1"] = "foo",
+				["prop2"] = new JsonArray
+				{
+					"bar",
+					new JsonObject
+					{
+						["type"] = "integer"
+					}
+				},
+				["prop3"] = new JsonObject
+				{
+					["$ref"] = "#/prop2/1"
+				}
+			};
+
+			var options = new EvaluationOptions
+			{
+				OutputFormat = OutputFormat.List
+			};
+
+			var jsonBaseDoc = new JsonNodeBaseDocument(json, new Uri("http://localhost:1234/doc"));
+			options.SchemaRegistry.Register(jsonBaseDoc);
+
+			JsonSchema subjectSchema = new JsonSchemaBuilder()
+				.Ref("http://localhost:1234/doc#/prop3");
+
+			JsonNode instance = 42;
+
+			var result = subjectSchema.Evaluate(instance, options);
+
+			result.IsValid.Should().BeTrue();
 		}
 	}
 }
