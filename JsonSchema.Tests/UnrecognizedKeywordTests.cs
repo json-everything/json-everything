900a004516cb89b7f5f598f9f9dabf425e781dfc
Greg Dennis <gregsdennis@yahoo.com>
2023-09-08 12:51:02 +1200

    add replicating test

--- JsonSchema.Tests/UnrecognizedKeywordTests.cs
+++ JsonSchema.Tests/UnrecognizedKeywordTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Json.More;
@@ -26,7 +28,49 @@ public class UnrecognizedKeywordTests
 
 		var schema = JsonSerializer.Deserialize<JsonSchema>(schemaText);
 
-		var result = schema!.Evaluate("{}", new EvaluationOptions { OutputFormat = OutputFormat.Hierarchical });
+		var result = schema!.Evaluate(new JsonObject(), new EvaluationOptions { OutputFormat = OutputFormat.Hierarchical });
+
+		Assert.IsTrue(result.IsValid);
+		Assert.AreEqual(1, result.Annotations!.Count);
+		Assert.IsTrue(((JsonNode?)"bar").IsEquivalentTo(result.Annotations.First().Value));
+	}
+
+	[Test]
+	public void FooProducesAnAnnotation_Constructed()
+	{
+		var schema = new JsonSchemaBuilder()
+			.Unrecognized("foo", "bar")
+			.Build();
+
+		var result = schema.Evaluate(new JsonObject(), new EvaluationOptions { OutputFormat = OutputFormat.Hierarchical });
+
+		Assert.IsTrue(result.IsValid);
+		Assert.AreEqual(1, result.Annotations!.Count);
+		Assert.IsTrue(((JsonNode?)"bar").IsEquivalentTo(result.Annotations.First().Value));
+	}
+
+	[Test]
+	public void FooProducesAnAnnotation_WithSchemaKeyword()
+	{
+		var options = new EvaluationOptions
+		{
+			OutputFormat = OutputFormat.Hierarchical
+		};
+		var schema = new JsonSchemaBuilder()
+			.Id("https://example.com")
+			.Schema(MetaSchemas.Draft202012Id)
+			.Unrecognized("foo", "bar")
+			.Build();
+
+		var instance = new JsonObject();
+
+		var result = schema.Evaluate(instance, options);
+
+		Console.WriteLine(JsonSerializer.Serialize(result, new JsonSerializerOptions
+		{
+			WriteIndented = true,
+			Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+		}));
 
 		Assert.IsTrue(result.IsValid);
 		Assert.AreEqual(1, result.Annotations!.Count);
