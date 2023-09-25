fb21b9e00d9023418ed4a4eaf4aa4ad12b178854
Jens-Uwe Jährig <jens-uwe.jaehrig@fsd-web.de>
2023-09-25 19:52:29 +0200

    convert PropertyNameResolver to a simple AutoProp via the suggested DummyInfo

--- JsonSchema.Generation/SchemaGeneratorConfiguration.cs
+++ JsonSchema.Generation/SchemaGeneratorConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using JetBrains.Annotations;
 using Json.Schema.Generation.Generators;
 
@@ -11,8 +12,6 @@ namespace Json.Schema.Generation;
 public class SchemaGeneratorConfiguration
 {
 	private PropertyNamingMethod? _propertyNamingMethod;
-	private PropertyNameResolver? _propertyNameResolver;
-
 	/// <summary>
 	/// A collection of refiners.
 	/// </summary>
@@ -38,7 +37,7 @@ public class SchemaGeneratorConfiguration
 	[Obsolete($"Use {nameof(PropertyNameResolver)} instead.")]
 	public PropertyNamingMethod? PropertyNamingMethod
 	{
-		get => _propertyNamingMethod;
+		get => x => PropertyNameResolver(new DummyInfo(x));
 		set => _propertyNamingMethod = value;
 	}
 
@@ -48,28 +47,7 @@ public class SchemaGeneratorConfiguration
 	/// <remarks>
 	/// This can be replaced with any `Func&lt;MemberInfo, string&gt;`.
 	/// </remarks>
-	public PropertyNameResolver PropertyNameResolver
-	{
-		get
-		{
-			// upwards compatibility: this field was actively set, this is only used logic.
-			if (_propertyNameResolver is PropertyNameResolver resolver)
-			{
-				return resolver;
-			}
-
-			// backwards compatibility: if the old property was actively set in existing code existing code, code will use it
-			if (PropertyNamingMethod is PropertyNamingMethod method)
-			{
-				return new PropertyNameResolver(x => method(x.Name));
-			}
-
-			// if neither property was actively set, the behavior is as it was before. Prefer JsonPropertyName and Fallback to AsDeclared
-			return PropertyNameResolvers.ByJsonPropertyName;
-		}
-
-		set => _propertyNameResolver = value;
-	}
+	public PropertyNameResolver PropertyNameResolver { get; set; } = PropertyNameResolvers.AsDeclared;
 
 	/// <summary>
 	/// Gets or sets whether to include `null` in the `type` keyword.
@@ -101,4 +79,27 @@ public class SchemaGeneratorConfiguration
 	[field: ThreadStatic]
 	public static SchemaGeneratorConfiguration Current { get; internal set; }
 #pragma warning restore CS8618
+
+	/// <summary>
+	/// A shim while <see cref="PropertyNamingMethod"/> is not yet removed.
+	/// Makes it Possible to call <see cref="PropertyNameResolver"/> from <see cref="PropertyNamingMethod"/>.
+	/// </summary>
+	private sealed class DummyInfo : MemberInfo
+	{
+		public override object[] GetCustomAttributes(bool inherit) => Array.Empty<object>();
+
+		public override object[] GetCustomAttributes(Type attributeType, bool inherit) => Array.Empty<object>();
+
+		public override bool IsDefined(Type attributeType, bool inherit) => false;
+
+		public override Type DeclaringType { get; } = typeof(DummyInfo);
+		public override MemberTypes MemberType { get; } = MemberTypes.Property;
+		public override string Name { get; }
+		public override Type? ReflectedType { get; } = null;
+
+		public DummyInfo(string name)
+		{
+			Name = name;
+		}
+	}
 }
