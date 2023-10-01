c09a8bf2ae85e1e088f5e32d4d9ede6ea092b781
Greg Dennis <gregsdennis@yahoo.com>
2023-10-02 08:57:22 +1300

    add cross-draft test

---
+++ JsonSchema.Tests/CrossDraftTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json.Nodes;
+using NUnit.Framework;
+
+namespace Json.Schema.Tests;
+
+public class CrossDraftTests
+{
+	[Test]
+	public void Test()
+	{
+		var schema = new JsonSchemaBuilder()
+			.Schema(MetaSchemas.Draft202012Id)
+			.Id("base")
+			.Type(SchemaValueType.Object)
+			.Properties(
+				("foo", new JsonSchemaBuilder().Ref("foo"))
+			)
+			.Defs(
+				("foo-def", new JsonSchemaBuilder()
+					.Schema(MetaSchemas.Draft7Id)
+					.Id("foo")
+					.Type(SchemaValueType.Array)
+					.Items(new JsonSchema[]
+					{
+						new JsonSchemaBuilder().Type(SchemaValueType.String),
+						new JsonSchemaBuilder().Type(SchemaValueType.Integer)
+					})
+				)
+			)
+			.Build();
+		var instance = new JsonObject
+		{
+			["foo"] = new JsonArray { "string", 42 }
+		};
+
+		var options = new EvaluationOptions
+		{
+			OutputFormat = OutputFormat.Hierarchical
+		};
+
+		var result = schema.Evaluate(instance, options);
+
+		result.AssertValid();
+	}
+}
