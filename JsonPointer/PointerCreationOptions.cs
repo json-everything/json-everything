a4086a3885e17d62175313c84c07da9af465c68a
Greg Dennis <gregsdennis@yahoo.com>
2023-10-28 16:28:15 +1300

    add xml comments

--- JsonPointer/PointerCreationOptions.cs
+++ JsonPointer/PointerCreationOptions.cs
@@ -1,11 +1,23 @@
-﻿namespace Json.Pointer;
+﻿using System;
+using System.Linq.Expressions;
 
+namespace Json.Pointer;
+
+/// <summary>
+/// Options for creating pointers using <see cref="JsonPointer.Create{T}(Expression{Func{T, object}}, PointerCreationOptions)"/>.
+/// </summary>
 public class PointerCreationOptions
 {
 	private PropertyNameResolver? _propertyNameResolver;
 	
-	public static PointerCreationOptions Default = new();
+	/// <summary>
+	/// Default settings.
+	/// </summary>
+	public static readonly PointerCreationOptions Default = new();
 
+	/// <summary>
+	/// Gets or sets the property naming resolver.  Default is <see cref="PropertyNameResolvers.AsDeclared"/>.
+	/// </summary>
 	public PropertyNameResolver? PropertyNameResolver
 	{
 		get => _propertyNameResolver ??= PropertyNameResolvers.AsDeclared;
