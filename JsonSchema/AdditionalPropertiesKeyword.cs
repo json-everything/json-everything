0b3ccd80be741a62f1542a1bdaae56f7559e24bb
Greg Dennis <gregsdennis@yahoo.com>
2023-11-22 10:05:04 +1300

    use generic attributes!

--- JsonSchema/AdditionalPropertiesKeyword.cs
+++ JsonSchema/AdditionalPropertiesKeyword.cs
@@ -21,8 +21,8 @@ namespace Json.Schema;
 [Vocabulary(Vocabularies.Applicator201909Id)]
 [Vocabulary(Vocabularies.Applicator202012Id)]
 [Vocabulary(Vocabularies.ApplicatorNextId)]
-[DependsOnAnnotationsFrom(typeof(PropertiesKeyword))]
-[DependsOnAnnotationsFrom(typeof(PatternPropertiesKeyword))]
+[DependsOnAnnotationsFrom<PropertiesKeyword>]
+[DependsOnAnnotationsFrom<PatternPropertiesKeyword>]
 [JsonConverter(typeof(AdditionalPropertiesKeywordJsonConverter))]
 public class AdditionalPropertiesKeyword : IJsonSchemaKeyword, ISchemaContainer
 {
--- JsonSchema/ItemsKeyword.cs
+++ JsonSchema/ItemsKeyword.cs
@@ -21,7 +21,7 @@ namespace Json.Schema;
 [Vocabulary(Vocabularies.Applicator201909Id)]
 [Vocabulary(Vocabularies.Applicator202012Id)]
 [Vocabulary(Vocabularies.ApplicatorNextId)]
-[DependsOnAnnotationsFrom(typeof(PrefixItemsKeyword))]
+[DependsOnAnnotationsFrom<PrefixItemsKeyword>]
 [JsonConverter(typeof(ItemsKeywordJsonConverter))]
 public class ItemsKeyword : IJsonSchemaKeyword, ISchemaContainer, ISchemaCollector
 {
