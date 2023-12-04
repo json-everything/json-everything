02a4665c4d00b3ec57044e1262c7df107b7cab60
Greg Dennis <gregsdennis@yahoo.com>
2023-12-04 13:19:57 +1300

    support yaml on schema playground

--- json-everything.net/Services/EditorExtensions.cs
+++ json-everything.net/Services/EditorExtensions.cs
@@ -1,4 +1,6 @@
-﻿using BlazorMonaco;
+﻿using System.Text.Json.Nodes;
+using BlazorMonaco;
+using Yaml2JsonNode;
 
 namespace JsonEverythingNet.Services;
 
@@ -9,9 +11,52 @@ public static class EditorExtensions
 		editor.GetModel()
 			.ContinueWith(x => MonacoEditorBase.SetModelLanguage(x.Result, language));
 	}
+
 	public static async Task SetLanguageAsync(this MonacoEditor editor, string language)
 	{
 		var model = await editor.GetModel();
 		await MonacoEditorBase.SetModelLanguage(model, language);
+	}
+
+	public static async Task DetectLanguage(this MonacoEditor editor)
+	{
+		var text = await editor.GetValue();
+
+		if (TryParseJson(text))
+		{
+			await editor.UpdateOptions(new GlobalEditorOptions { TabSize = 2 });
+			await editor.SetLanguageAsync("json");
+		}
+		else if (TryParseYaml(text))
+		{
+			await editor.UpdateOptions(new GlobalEditorOptions { TabSize = 2 });
+			await editor.SetLanguageAsync("yaml");
+		}
+	}
+
+	private static bool TryParseJson(string text)
+	{
+		try
+		{
+			JsonNode.Parse(text);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+
+	private static bool TryParseYaml(string text)
+	{
+		try
+		{
+			YamlSerializer.Parse(text);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
 	}
 }
