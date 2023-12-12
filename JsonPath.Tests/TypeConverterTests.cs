7dae710d703063d1e38710d8ce0b0f480cdceab9
Graham Watts <grahamcharleswatts@gmail.com>
2023-12-12 14:18:44 +0200

    Add JsonPath type converter

---
+++ JsonPath.Tests/TypeConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+
+namespace Json.Path.Tests
+{
+	public class TypeConverterTests
+	{
+
+		[Test]
+		public void ConvertFromString()
+		{
+			var typeConverter = TypeDescriptor.GetConverter(typeof(JsonPath));
+			var path = typeConverter.ConvertFromInvariantString("$.store.book[*].author") as JsonPath;
+
+			Assert.IsNotNull(path);
+			Assert.AreEqual("$.store.book[*].author", path!.ToString());
+		}
+
+		[Test]
+		public void ConvertToString()
+		{
+			var path = JsonPath.Parse("$.store.book[*].author");
+			var typeConverter = TypeDescriptor.GetConverter(typeof(JsonPath));
+			var pathString = typeConverter.ConvertToInvariantString(path);
+
+			Assert.AreEqual("$.store.book[*].author", pathString);
+		}
+
+	}
+}
---
+++ JsonPath/JsonPathTypeConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Json.Path
+{
+	/// <summary>
+	/// <see cref="TypeConverter"/> for <see cref="JsonPath"/>.
+	/// </summary>
+	internal sealed class JsonPathTypeConverter : TypeConverter
+	{
+
+		/// <inheritdoc/>
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType == typeof(string);
+		}
+
+		/// <inheritdoc/>
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			return destinationType == typeof(string);
+		}
+
+		/// <inheritdoc/>
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			if (value is string s)
+			{
+				return JsonPath.Parse(s);
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+
+		/// <inheritdoc/>
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if (destinationType == typeof(string) && value is JsonPath pointer)
+			{
+				return pointer.ToString();
+			}
+
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+
+	}
+}
