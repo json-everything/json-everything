4792d730326b9dba788bdf0a57045a546389ca3a
Graham Watts <grahamcharleswatts@gmail.com>
2023-12-12 14:13:57 +0200

    Add JsonPointer type converter

---
+++ JsonPointer.Tests/TypeConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using NUnit.Framework;
+
+namespace Json.Pointer.Tests
+{
+  public class TypeConverterTests
+  {
+
+		[Test]
+		public void ConvertFromString()
+		{
+			var typeConverter = TypeDescriptor.GetConverter(typeof(JsonPointer));
+			var pointer = typeConverter.ConvertFromInvariantString("/foo") as JsonPointer;
+
+			Assert.IsNotNull(pointer);
+			Assert.AreEqual("/foo", pointer!.ToString());
+		}
+
+		[Test]
+		public void ConvertToString()
+		{
+			var pointer = JsonPointer.Parse("/foo");
+			var typeConverter = TypeDescriptor.GetConverter(typeof(JsonPointer));
+			var pointerString = typeConverter.ConvertToInvariantString(pointer);
+
+			Assert.AreEqual("/foo", pointerString);
+		}
+
+  }
+}
---
+++ JsonPointer/JsonPointerTypeConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Json.Pointer
+{
+	/// <summary>
+	/// <see cref="TypeConverter"/> for <see cref="JsonPointer"/>.
+	/// </summary>
+	internal sealed class JsonPointerTypeConverter : TypeConverter
+	{
+
+		/// <inheritdoc/>
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+			return sourceType == typeof(string);
+		}
+
+		/// <inheritdoc/>
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			return destinationType == typeof(string);
+		}
+
+		/// <inheritdoc/>
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+			if (value is string s)
+			{
+				return JsonPointer.Parse(s);
+			}
+
+			return base.ConvertFrom(context, culture, value);
+		}
+
+		/// <inheritdoc/>
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if (destinationType == typeof(string) && value is JsonPointer pointer)
+			{
+				return pointer.ToString();
+			}
+
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
+
+	}
+}
