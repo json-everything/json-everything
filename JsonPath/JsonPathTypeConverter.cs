1ba29b488eab95577d83fd8d7f01dba21f779246
Graham Watts <grahamcharleswatts@gmail.com>
2023-12-13 09:13:48 +0200

    Override additional TypeConverter methods
    Implements additional `TypeConverter` methods as requested in https://github.com/gregsdennis/json-everything/pull/586#pullrequestreview-1778280696
    
    Implementation of additional methods is largely based on the BCL implementation of [UriTypeConverter](https://github.com/dotnet/runtime/blob/main/src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/UriTypeConverter.cs), since this was the closest parallel I could think of.
    
    Note that `UriTypeConverter` does not override the base `GetCreateInstanceSupported` or `CreateInstance` implementations, so I have not done so here either. I have, however, added support for `JsonPointer` => `JsonPointer` and `JsonPath` => `JsonPath` conversions; this is implemented in `ConvertFrom` and `ConvertTo` in a similar way to `UriTypeConverter`.

--- JsonPath/JsonPathTypeConverter.cs
+++ JsonPath/JsonPathTypeConverter.cs
@@ -4,22 +4,30 @@ using System.Globalization;
 
 namespace Json.Path
 {
-	/// <summary>
-	/// <see cref="TypeConverter"/> for <see cref="JsonPath"/>.
-	/// </summary>
-	internal sealed class JsonPathTypeConverter : TypeConverter
+  /// <summary>
+  /// <see cref="TypeConverter"/> for <see cref="JsonPath"/>.
+  /// </summary>
+  internal sealed class JsonPathTypeConverter : TypeConverter
 	{
+
+		/// <summary>
+		/// The standard values used by this converter.
+		/// </summary>
+		/// <remarks>
+		///   This field is initialized using a so-called poor man's lazy in <see cref="GetStandardValues(ITypeDescriptorContext)"/>.
+		/// </remarks>
+		private static StandardValuesCollection? _standardValues;
 
 		/// <inheritdoc/>
 		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
 		{
-			return sourceType == typeof(string);
+			return sourceType == typeof(string) || sourceType == typeof(JsonPath) || base.CanConvertFrom(context, sourceType);
 		}
 
 		/// <inheritdoc/>
 		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
 		{
-			return destinationType == typeof(string);
+			return destinationType == typeof(string) || destinationType == typeof(JsonPath) || base.CanConvertTo(context, destinationType);
 		}
 
 		/// <inheritdoc/>
@@ -29,19 +37,58 @@ namespace Json.Path
 			{
 				return JsonPath.Parse(s);
 			}
+			else if (value is JsonPath path)
+			{
+				return new JsonPath(path.Scope, path.Segments);
+			}
 
-			return base.ConvertFrom(context, culture, value);
+			throw GetConvertFromException(value);
 		}
 
 		/// <inheritdoc/>
 		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
 		{
-			if (destinationType == typeof(string) && value is JsonPath pointer)
+			if (value is JsonPath path)
 			{
-				return pointer.ToString();
+				if (destinationType == typeof(string))
+				{
+					return path.ToString();
+				}
+				else if (destinationType == typeof(JsonPath))
+				{
+					return new JsonPath(path.Scope, path.Segments);
+				}
 			}
 
-			return base.ConvertTo(context, culture, value, destinationType);
+			throw GetConvertToException(value, destinationType);
+		}
+
+		/// <inheritdoc/>
+		public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+		{
+			return true;
+		}
+
+		/// <inheritdoc/>
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+		{
+			return false;
+		}
+
+		/// <inheritdoc/>
+		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			return _standardValues ??= new StandardValuesCollection(new[] { JsonPath.Root });
+		}
+
+		/// <inheritdoc/>
+		public override bool IsValid(ITypeDescriptorContext context, object value)
+		{
+			if (value is string s)
+			{
+				return JsonPath.TryParse(s, out _);
+			}
+			return value is JsonPath;
 		}
 
 	}
--- JsonPointer/JsonPointerTypeConverter.cs
+++ JsonPointer/JsonPointerTypeConverter.cs
@@ -4,22 +4,30 @@ using System.Globalization;
 
 namespace Json.Pointer
 {
-	/// <summary>
-	/// <see cref="TypeConverter"/> for <see cref="JsonPointer"/>.
-	/// </summary>
-	internal sealed class JsonPointerTypeConverter : TypeConverter
+  /// <summary>
+  /// <see cref="TypeConverter"/> for <see cref="JsonPointer"/>.
+  /// </summary>
+  internal sealed class JsonPointerTypeConverter : TypeConverter
 	{
+
+		/// <summary>
+		/// The standard values used by this converter.
+		/// </summary>
+		/// <remarks>
+		///   This field is initialized using a so-called poor man's lazy in <see cref="GetStandardValues(ITypeDescriptorContext)"/>.
+		/// </remarks>
+		private static StandardValuesCollection? _standardValues;
 
 		/// <inheritdoc/>
 		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
 		{
-			return sourceType == typeof(string);
+			return sourceType == typeof(string) || sourceType == typeof(JsonPointer) || base.CanConvertFrom(context, sourceType);
 		}
 
 		/// <inheritdoc/>
 		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
 		{
-			return destinationType == typeof(string);
+			return destinationType == typeof(string) || destinationType == typeof(JsonPointer) || base.CanConvertTo(context, destinationType);
 		}
 
 		/// <inheritdoc/>
@@ -29,20 +37,59 @@ namespace Json.Pointer
 			{
 				return JsonPointer.Parse(s);
 			}
+			else if (value is JsonPointer pointer)
+			{
+				return JsonPointer.Create(pointer.Segments);
+			}
 
-			return base.ConvertFrom(context, culture, value);
+			throw GetConvertFromException(value);
 		}
 
 		/// <inheritdoc/>
 		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
 		{
-			if (destinationType == typeof(string) && value is JsonPointer pointer)
+			if (value is JsonPointer pointer)
 			{
-				return pointer.ToString();
+				if (destinationType == typeof(string))
+				{
+					return pointer.ToString();
+				}
+				else if (destinationType == typeof(JsonPointer))
+				{
+					return JsonPointer.Create(pointer.Segments);
+				}
 			}
 
-			return base.ConvertTo(context, culture, value, destinationType);
+			throw GetConvertToException(value, destinationType);
 		}
 
-	}
+		/// <inheritdoc/>
+		public override bool GetStandardValuesSupported(ITypeDescriptorContext context)
+		{
+			return true;
+		}
+
+		/// <inheritdoc/>
+		public override bool GetStandardValuesExclusive(ITypeDescriptorContext context)
+		{
+			return false;
+		}
+
+		/// <inheritdoc/>
+		public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+		{
+			return _standardValues ??= new StandardValuesCollection(new[] { JsonPointer.Empty });
+		}
+
+		/// <inheritdoc/>
+		public override bool IsValid(ITypeDescriptorContext context, object value)
+		{
+			if (value is string s)
+			{
+				return JsonPointer.TryParse(s, out _);
+			}
+			return value is JsonPointer;
+		}
+
+  }
 }
