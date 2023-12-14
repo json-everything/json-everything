49c99d0e446464de994fd5c0c65bafe49cbedcab
Greg Dennis <gregsdennis@yahoo.com>
2023-12-14 15:00:55 +1300

    remove 'quickness' test as it's ruining gh actions build times

--- JsonPath.Tests/DeferredExecutionTests.cs
+++ JsonPath.Tests/DeferredExecutionTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Text.Json.Nodes;
-using Json.More;
+﻿using System.Text.Json.Nodes;
 using NUnit.Framework;
 
 namespace Json.Path.Tests;
@@ -22,34 +18,5 @@ public class DeferredExecutionTests
 		data[0] = "sally";
 
 		Assert.AreEqual(2, result.Matches!.Count);
-	}
-
-	[Test]
-	public void GettingFirstMatchIsQuick()
-	{
-		var data = Enumerable.Range(0, 1000000).Select(x => (JsonNode?)x).ToJsonArray();
-		var query = JsonPath.Parse("$[? @ > 0 ]");
-
-		var result = query.Evaluate(data);
-		var first = result.Matches!.First();
-
-		var sw = new Stopwatch();
-		sw.Start();
-		Assert.AreEqual(1.0, first.Value!.AsValue().GetNumber());
-		sw.Stop();
-
-		var shortcutted = sw.ElapsedMilliseconds;
-
-		sw.Reset();
-		sw.Start();
-		var asList = result.Matches!.ToList();
-		first = asList.First();
-		Assert.AreEqual(1.0, first.Value!.AsValue().GetNumber());
-		sw.Stop();
-
-		Console.WriteLine($"items in full results: {asList.Count}");
-		Console.WriteLine($"Time for shortcutted query: {shortcutted}ms");
-		Console.WriteLine($"Time for full evaluation:   {sw.ElapsedMilliseconds}ms");
-		Assert.Less(shortcutted, sw.ElapsedMilliseconds);
 	}
 }
