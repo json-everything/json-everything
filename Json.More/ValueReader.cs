026f20d355fabcd09639824a5ee8ec55c17c40b4
Greg Dennis <gregsdennis@yahoo.com>
2024-01-21 15:27:08 +1300

    remove compiler directives and r# type name warnings

--- Json.More/ValueReader.cs
+++ Json.More/ValueReader.cs
@@ -6,22 +6,14 @@ namespace Json.More;
 
 internal static class ValueReader
 {
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static ValueTuple<T> ReadValues1<T>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T>(ref enumerator, options);
 
 		return new ValueTuple<T>(value1);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2) ReadValues2<T1, T2>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -29,11 +21,7 @@ internal static class ValueReader
 		return (value1, value2);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2, T3) ReadValues3<T1, T2, T3>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -42,11 +30,7 @@ internal static class ValueReader
 		return (value1, value2, value3);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2, T3, T4) ReadValues4<T1, T2, T3, T4>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -56,11 +40,7 @@ internal static class ValueReader
 		return (value1, value2, value3, value4);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2, T3, T4, T5) ReadValues5<T1, T2, T3, T4, T5>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -71,11 +51,7 @@ internal static class ValueReader
 		return (value1, value2, value3, value4, value5);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2, T3, T4, T5, T6) ReadValues6<T1, T2, T3, T4, T5, T6>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -87,11 +63,7 @@ internal static class ValueReader
 		return (value1, value2, value3, value4, value5, value6);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static (T1, T2, T3, T4, T5, T6, T7) ReadValues7<T1, T2, T3, T4, T5, T6, T7>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
-		// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
@@ -104,12 +76,8 @@ internal static class ValueReader
 		return (value1, value2, value3, value4, value5, value6, value7);
 	}
 
-#pragma warning disable IDE1006
-	// ReSharper disable InconsistentNaming
 	public static ValueTuple<T1, T2, T3, T4, T5, T6, T7, TRest> ReadValues8<T1, T2, T3, T4, T5, T6, T7, TRest>(JsonElement.ArrayEnumerator enumerator, JsonSerializerOptions options)
 		where TRest : struct
-// ReSharper restore InconsistentNaming
-#pragma warning restore IDE1006
 	{
 		var value1 = JsonElementExtensions.ReadValue<T1>(ref enumerator, options);
 		var value2 = JsonElementExtensions.ReadValue<T2>(ref enumerator, options);
