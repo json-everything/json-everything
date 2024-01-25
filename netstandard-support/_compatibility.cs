0cc2e03334029f578a4534678929dfda04d376af
Greg Dennis <gregsdennis@yahoo.com>
2024-01-26 10:50:12 +1300

    reintroduce IsEquivalentTo() for .net 8

--- netstandard-support/_compatibility.cs
+++ netstandard-support/_compatibility.cs
@@ -6,11 +6,6 @@ namespace Json.More
 {
 	internal static class JsonNodeExtensions
 	{
-		internal static bool IsEquivalentTo(this JsonNode? a, JsonNode? b)
-		{
-			return JsonNode.DeepEquals(a, b);
-		}
-
 		internal static JsonNode? Copy(this JsonNode? a)
 		{
 			return a?.DeepClone();
