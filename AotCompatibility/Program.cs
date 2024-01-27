4a4c190af3e2e24e479bdb295ac4e5ea8c200eb6
Greg Dennis <gregsdennis@yahoo.com>
2024-01-27 20:24:08 +1300

    add test app

---
+++ AotCompatibility/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Json.Schema;
+
+var schema = JsonSchema.FromText("""
+                                 {
+                                   "$schema": "https://json-schema.org/draft/2020-12/schema",
+                                   "type": "object",
+                                   "properties": {
+                                     "foo": { "type": "integer" }
+                                   }
+                                 }
+                                 """);
+
+var instance = JsonNode.Parse("{\"foo\": 1}");
+
+var result = schema.Evaluate(instance, new EvaluationOptions { OutputFormat = OutputFormat.Hierarchical });
+
+var serializerOptions = new JsonSerializerOptions();
+serializerOptions.TypeInfoResolverChain.Add(JsonSchema.TypeInfoResolver);
+
+Console.WriteLine("Schema:");
+Console.WriteLine(JsonSerializer.Serialize(schema, serializerOptions.GetTypeInfo(typeof(JsonSchema))));
+
+Console.WriteLine("Result:");
+Console.WriteLine(JsonSerializer.Serialize(result, serializerOptions.GetTypeInfo(typeof(EvaluationResults))));
