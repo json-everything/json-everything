5b8fd159d953a703bc82854755573b408f123d62
Greg Dennis <gregsdennis@yahoo.com>
2024-02-21 09:44:39 +1300

    add failing test

---
+++ JsonSchema.DataGeneration.Tests/RefTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+using static Json.Schema.DataGeneration.Tests.TestHelpers;
+
+namespace Json.Schema.DataGeneration.Tests;
+
+internal class RefTests
+{
+	[Test]
+	public void PointerRef()
+	{
+		var schema = JsonSchema.FromText(
+			"""
+			{
+			    "type": "array",
+			    "items": { "$ref": "#/$defs/positiveInteger" },
+			    "$defs": {
+			        "positiveInteger": {
+			            "type": "integer",
+			            "exclusiveMinimum": 0
+			        }
+			    },
+			    "minItems": 2,
+			    "additionalProperties" : false
+			}
+			""");
+
+		Run(schema);
+	}
+}
