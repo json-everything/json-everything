d03a2344c6a6498826385849292013045c555233
Greg Dennis <gregsdennis@yahoo.com>
2024-04-18 13:06:02 +1200

    clear warning

--- Benchmarks/LogicSuite/SuiteRunner.cs
+++ Benchmarks/LogicSuite/SuiteRunner.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
 using Json.Logic;
 using Json.More;
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace Json.Benchmarks.LogicSuite;
 
