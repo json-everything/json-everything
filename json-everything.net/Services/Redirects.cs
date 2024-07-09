a1b48a0063599aa973e4becbf1a6fa2f8feda8a9
Greg Dennis <gregsdennis@yahoo.com>
2024-07-09 17:46:52 +1200

    add tooltips to icon header links

--- json-everything.net/Services/Redirects.cs
+++ json-everything.net/Services/Redirects.cs
@@ -4,5 +4,5 @@ public static class Redirects
 {
 	public const string Github = "https://github.com/json-everything/json-everything";
 
-	public const string Slack = "https://join.slack.com/t/manateeopensource/shared_invite/enQtMzU4MjgzMjgyNzU3LWZjYzAzYzY3NjY1MjY3ODI0ZGJiZjc3Nzk1MDM5NTNlMjMyOTE0MzMxYWVjMjdiOGU1NDY5OGVhMGQ5YzY4Zjg";
+	public const string Slack = "https://join.slack.com/t/json-everything/shared_invite/zt-4klcm69x-_MA8Z2ZSZ4JNu_d3lQOVPg";
 }
