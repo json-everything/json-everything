dd525674bce52cd41308c2a52b4f3b727429042e
Greg Dennis <gregsdennis@yahoo.com>
2024-08-06 11:07:23 +1200

    add invalid pattern deserialize tests

---
+++ src/JsonSchema.Tests/PatternTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using NUnit.Framework;
+
+namespace Json.Schema.Tests;
+
+public class PatternTests
+{
+	[Test]
+	public void DeserializingInvalidPatternDoesNotThrow()
+	{
+		var schemaText =
+			"""
+			   {
+			  "pattern": "(.{2}"
+			}
+			""";
+
+		var schema = JsonSerializer.Deserialize(schemaText, TestSerializerContext.Default.JsonSchema)!;
+
+		JsonNode instance = "some string";
+
+		var result = schema.Evaluate(instance);
+
+		result.AssertInvalid();
+	}
+	[Test]
+	public void DeserializingInvalidPatternPropertyDoesNotThrow()
+	{
+		var schemaText =
+			"""
+			{
+			  "patternProperties": {
+				"(.{2}": true
+			  }
+			}
+			""";
+
+		var schema = JsonSerializer.Deserialize(schemaText, TestSerializerContext.Default.JsonSchema)!;
+
+		var instance = JsonNode.Parse(
+			"""
+			{
+			  "abc": 42
+			}
+			""");
+
+		var result = schema.Evaluate(instance);
+
+		result.AssertValid(); // property doesn't match because it's invalid
+	}
+}
