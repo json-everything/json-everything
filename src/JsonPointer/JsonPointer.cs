4df091f167dab239d29a7a322467d3caf4291def
Alexander Jesner <alexander@jesner.eu>
2024-12-27 12:59:30 +0100

    JsonPointer: use 'params ReadOnlySpan' to avoid array allocation when creating from segments

--- src/JsonPointer/JsonPointer.cs
+++ src/JsonPointer/JsonPointer.cs
@@ -205,6 +205,9 @@ public class JsonPointer : IEquatable<JsonPointer>, IReadOnlyList<string>
 	/// <remarks>This method creates un-encoded pointers only.</remarks>
 	public static JsonPointer Create(params PointerSegment[] segments)
 	{
+#if NET9_0_OR_GREATER
+		return Create(segments.AsSpan());
+#else
 		var array = new string[segments.Length];
 
 		for (var i = 0; i < segments.Length; i++)
@@ -213,7 +216,30 @@ public class JsonPointer : IEquatable<JsonPointer>, IReadOnlyList<string>
 		}
 
 		return new JsonPointer(array);
+#endif
 	}
+
+#if NET9_0_OR_GREATER
+
+	/// <summary>
+	/// Creates a new JSON Pointer from a collection of segments.
+	/// </summary>
+	/// <param name="segments">A collection of segments.</param>
+	/// <returns>The JSON Pointer.</returns>
+	/// <remarks>This method creates un-encoded pointers only.</remarks>
+	public static JsonPointer Create(params ReadOnlySpan<PointerSegment> segments)
+	{
+		var array = new string[segments.Length];
+		
+		for (var i = 0; i < segments.Length; i++)
+		{
+			array[i] = segments[i].Value;
+		}
+
+		return new JsonPointer(array);
+	}
+
+#endif
 
 	/// <summary>
 	/// Generates a JSON Pointer from a lambda expression.
@@ -295,7 +321,11 @@ public class JsonPointer : IEquatable<JsonPointer>, IReadOnlyList<string>
 
 		i++;
 
+#if NET9_0_OR_GREATER
+		return Create(segments[i..]);
+#else
 		return Create(segments[i..].ToArray());
+#endif
 	}
 
 	/// <summary>
