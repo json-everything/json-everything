09959c0a4d17f03a78cb67c24e222756963a88c8
Greg Dennis <gregsdennis@yahoo.com>
2025-11-29 21:47:01 +1300

    disallow unknown keywords

--- src/JsonSchema/MetaSchemas.cs
+++ src/JsonSchema/MetaSchemas.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+// ReSharper disable InconsistentNaming
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 
 namespace Json.Schema;
 
