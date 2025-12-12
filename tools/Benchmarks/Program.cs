340f5e10067d8068e503c15d7371626feaf228e6
Greg Dennis <gregsdennis@yahoo.com>
2025-12-13 11:03:06 +1300

    reset benchmark

--- tools/Benchmarks/Program.cs
+++ tools/Benchmarks/Program.cs
@@ -7,7 +7,6 @@ using BenchmarkDotNet.Running;
 using Json.Benchmarks.LogicSuite;
 using Json.Benchmarks.Pointer;
 using Json.Benchmarks.SchemaSuite;
-using Json.Schema;
 
 namespace Json.Benchmarks;
 
@@ -20,32 +19,11 @@ class Program
 		//config.WithOptions(ConfigOptions.DisableOptimizationsValidator);
 		//var summary = BenchmarkRunner.Run<TestSuiteRunner>(config);
 
-		//var runner = new SingleSchemaRunner();
-		//while (true)
-		//{
-		//	runner.BuildOnce(1000);
-		//}
-
-		var stopwatch = new Stopwatch();
-
-		stopwatch.Start();
-		var schema = JsonSchema.FromFile(@"C:\Users\gregs\Downloads\schema_rand.json");
-		stopwatch.Stop();
-		Console.WriteLine($"Schema loaded: {stopwatch.ElapsedMilliseconds}ms");
-
-		stopwatch.Reset();
-		stopwatch.Start();
-		var instance = JsonDocument.Parse(File.ReadAllText(@"C:\Users\gregs\Downloads\schema_data_rand.json")).RootElement;
-		stopwatch.Stop();
-		Console.WriteLine($"Instance loaded: {stopwatch.ElapsedMilliseconds}ms");
-
-		stopwatch.Reset();
-		stopwatch.Start();
-		var result = schema.Evaluate(instance);
-		stopwatch.Stop();
-		Console.WriteLine($"Evaluation complete: {result.IsValid}, {stopwatch.ElapsedMilliseconds/1000.0}s");
-		
-		Console.ReadLine();
+		var runner = new SingleSchemaRunner();
+		while (true)
+		{
+			runner.BuildOnce(1000);
+		}
 #else
 		var summary = BenchmarkRunner.Run<SingleSchemaRunner>();
 #endif
